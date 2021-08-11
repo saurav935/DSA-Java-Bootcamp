7158bc3411bbd5d43b287a2b9d045a1ee12e0825
saurav <75733364+saurav935@users.noreply.github.com>
2021-08-11 18:59:32 +0530

    Update Build Array from Permutation.py

--- Leetcode assignments/Build Array from Permutation.py
+++ Leetcode assignments/Build Array from Permutation.py
@@ -2,7 +2,7 @@
 # Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
 # A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
 
-[Building Array from permutation](https://leetcode.com/problems/build-array-from-permutation/)
+# Building Array from permutation(https://leetcode.com/problems/build-array-from-permutation/)
 
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
