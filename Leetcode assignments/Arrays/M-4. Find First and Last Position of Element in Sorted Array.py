471f2a10131556fcfb3cfb6cab442e241fb975fb
saurav <75733364+saurav935@users.noreply.github.com>
2021-08-16 23:17:08 +0530

    Add files via upload

---
+++ Leetcode assignments/Arrays/M-4. Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,57 @@
+
+
+def searchRange(nums,target):
+    if nums == []:
+        return [-1 ,-1]
+
+    l = 0
+    r = len(nums) - 1
+
+    while l < r:
+        mid = ( l +r) // 2
+
+        if nums[mid] == target:
+            while nums[l] != target:
+                l += 1
+
+            while nums[r] != target:
+                r -= 1
+
+            if nums[l] == target and nums[r] == target:
+                return [l ,r]
+
+        elif nums[mid] < target:
+            l = mid + 1
+
+        elif nums[mid] > target:
+            r = mid - 1
+
+    if nums[l] == target and nums[r] == target:
+        return [l ,r]
+    else:
+        return [-1 ,-1]
+
+
+
+
+#  Time complexity - O(n)
+def searchRange(nums,target):
+
+    n = len(nums)
+    arr = []
+    arr1 = []
+
+    if target not in nums:
+        return [-1,-1]
+
+    for i in range(0,n):
+        if nums[i] == target:
+            arr.append(i)
+        else:
+            continue
+
+
+    arr1.append(min(arr))
+    arr1.append(max(arr))
+
+    return arr1
