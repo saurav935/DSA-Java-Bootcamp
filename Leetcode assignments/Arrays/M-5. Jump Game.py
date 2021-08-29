b262fe5a166e0f7a1087bbd7c215804186b17064
saurav <75733364+saurav935@users.noreply.github.com>
2021-08-29 08:53:45 +0530

    Add files via upload

---
+++ Leetcode assignments/Arrays/M-5. Jump Game.py
@@ -0,0 +1,29 @@
+
+# Time - O(n)
+# Space - O(1)
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_reach = 0
+
+        for i in range(0 ,len(nums)):
+            if i > max_reach:
+                return False
+
+            max_reach = max(max_reach, i+ nums[i])
+
+        return True
+
+
+# Time - O(n)
+# Space - O(1)
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        i = 0
+        n = len(nums)
+        reach = 0
+
+        while i < n and i <= reach:
+            reach = max(i + nums[i], reach)
+            i += 1
+
+        return i == n
