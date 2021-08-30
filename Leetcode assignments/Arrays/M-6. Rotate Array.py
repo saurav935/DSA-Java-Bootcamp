8bb6a6edc5a63f2d802be0027ae6bbe3cf347211
saurav <75733364+saurav935@users.noreply.github.com>
2021-08-30 15:24:17 +0530

    Update M-6. Rotate Array.py

--- Leetcode assignments/Arrays/M-6. Rotate Array.py
+++ Leetcode assignments/Arrays/M-6. Rotate Array.py
@@ -18,10 +18,7 @@ class Solution:
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-
         k = k % len(nums)
-
         nums[:] = nums[len(nums) - k:] + nums[:len(nums) - k]
+
+        
