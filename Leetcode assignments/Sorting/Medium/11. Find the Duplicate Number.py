756c39f396833e57d522f97ff3d75caabd7b5d51
saurav <75733364+saurav935@users.noreply.github.com>
2021-10-06 10:34:46 +0530

    Update 11. Find the Duplicate Number.py

--- Leetcode assignments/Sorting/Medium/11. Find the Duplicate Number.py
+++ Leetcode assignments/Sorting/Medium/11. Find the Duplicate Number.py
@@ -1,4 +1,28 @@
+# Amazon interview question
+# Using cyclic sort
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] != nums[nums[i]-1]:
+                self.swap(nums,i,nums[i]-1)
+            else:
+                i += 1
+                
+        for j in range(0,len(nums)):
+            if nums[j] != j+1:
+                return nums[j]
+            
+    
+    def swap(self,arr,i,j):
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+        
 
+
+
+        
 # Linear time and constant space
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
