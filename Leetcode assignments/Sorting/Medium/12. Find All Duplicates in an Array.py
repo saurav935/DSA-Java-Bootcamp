13412a267f472becb96d80f9ce933788be6bb3a9
saurav <75733364+saurav935@users.noreply.github.com>
2021-10-06 11:07:19 +0530

    Update 12. Find All Duplicates in an Array.py

--- Leetcode assignments/Sorting/Medium/12. Find All Duplicates in an Array.py
+++ Leetcode assignments/Sorting/Medium/12. Find All Duplicates in an Array.py
@@ -1,28 +1,58 @@
-
+# Using cyclic sort
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        majority = nums[0]
-        count = 1
-        times = len(nums) // 2
-
-        for i in nums:
-            if majority == i:
-                count += 1
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        i = 0
+        while i < len(nums):
+            if nums[i] != nums[nums[i]-1]:
+                self.swap(nums,i,nums[i]-1)
             else:
-                count -= 1
-                if count == 0:
-                    majority = i
-                    count = 1
+                i += 1
+                
+        arr = []
+        
+        for j in range(0,len(nums)):
+            if nums[j] != j+1:
+                arr.append(nums[j])
+        
+        return arr
+        
+        
+    
+    def swap(self,arr,i,j):
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+        
+        
+        
+        
 
-        return majority
-
-
-# Using sorting
-
+# Time complexity - O(n)
+# Space complexity - O(1)
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums) // 2]
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        arr = []
+        
+        for i in nums:
+            if nums[abs(i)-1] < 0:
+                arr.append(abs(i))
+            else:
+                nums[abs(i)-1] *= -1
+                
+        return arr
+        
+        
+        
 
-
-
+# Time complexity - O(n)
+# Space complexity - O(n)
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        count = Counter(nums)
+        arr = []
+        
+        for key,value in count.items():
+            if value == 2:
+                arr.append(key)
+                
+        return arr
