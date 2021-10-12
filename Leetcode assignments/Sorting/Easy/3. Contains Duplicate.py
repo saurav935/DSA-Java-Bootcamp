495cca5b7bc0336d7b2471c38b77dc979f3604af
saurav <75733364+saurav935@users.noreply.github.com>
2021-10-12 16:58:00 +0530

    Add files via upload

---
+++ Leetcode assignments/Sorting/Easy/3. Contains Duplicate.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+
+        for i in range(0, len(nums) - 1):
+            if nums[i] == nums[i + 1]:
+                return True
+
+        return False
