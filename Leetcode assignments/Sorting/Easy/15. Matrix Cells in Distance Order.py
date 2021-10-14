1566f7765dcaf551919269acbf71805a6e2fe3d7
saurav <75733364+saurav935@users.noreply.github.com>
2021-10-14 15:04:14 +0530

    Create 15. Matrix Cells in Distance Order.py

---
+++ Leetcode assignments/Sorting/Easy/15. Matrix Cells in Distance Order.py
@@ -0,0 +1,23 @@
+class Solution:
+    def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
+        coordinates = []
+        dist = []
+        hashmap = defaultdict(list)
+        
+        for r in range(0,rows):
+            for c in range(0,cols):
+                coordinates.append([r,c])
+                
+
+        for i in coordinates:
+            distance = abs(i[0] - rCenter) + abs(i[1] - cCenter)
+            dist.append(distance)
+            hashmap[distance].append(i)
+            
+        arr = []
+        dist = sorted(list(set(dist)))
+        
+        for i in dist:
+            arr.extend(hashmap[i])
+
+        return arr
