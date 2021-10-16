455a92a72a22ff700cd3758afb84c6399b4c839d
saurav <75733364+saurav935@users.noreply.github.com>
2021-10-16 21:34:17 +0530

    Create 2. Shuffle String.py

---
+++ Leetcode assignments/Strings/2. Shuffle String.py
@@ -0,0 +1,9 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        ans = [""] * len(indices)
+        
+        for i in range(0,len(indices)):
+            ans[indices[i]] = s[i]
+            
+        return "".join(ans)
+        
