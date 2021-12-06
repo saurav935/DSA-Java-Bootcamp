cbeacda469465f6ebf05401079f31c097a37b8a2
saurav <75733364+saurav935@users.noreply.github.com>
2021-12-06 23:36:59 +0530

    Create 2. Single Number.py

---
+++ Leetcode assignments/Bitwise/Easy/2. Single Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        unique = 0
+        
+        for i in nums:
+            unique ^= i
+            
+        return unique
+        
