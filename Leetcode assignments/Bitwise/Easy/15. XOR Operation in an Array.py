3d057b2184a8730feb4c183e837bbeec95f7ac02
saurav <75733364+saurav935@users.noreply.github.com>
2021-12-20 23:42:12 +0530

    Update 15. XOR Operation in an Array.py

--- Leetcode assignments/Bitwise/Easy/15. XOR Operation in an Array.py
+++ Leetcode assignments/Bitwise/Easy/15. XOR Operation in an Array.py
@@ -4,5 +4,13 @@ class Solution:
         ans = arr[0]
         for num in arr[1:]:
             ans = ans ^ num
-            
+        return ans
+
+# OR
+
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+        ans = 0
+        for i in range(0,n):
+            ans ^= start+2*i            
         return ans
